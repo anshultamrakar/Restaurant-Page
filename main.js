8b4fd2aabb7b4f0f12503c044d94351a71717bee
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-07 16:01:08 -0400

    final commit

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-    initApp();
-  });
-  
-  const initApp = () => {
-    const menuBtn = document.querySelector(".nav__burger-btn");
-    const list = document.querySelector(".nav__list");
-    menuBtn.addEventListener("click", toggleButtonAndMenu);
-    list.addEventListener("click", toggleButtonAndMenu);
-    displayCurrentYear();
-  };
-  
-  const toggleButtonAndMenu = () => {
-    const menuBtn = document.querySelector(".nav__burger-btn");
-    const list = document.querySelector(".nav__list");
-    menuBtn.classList.toggle("open");
-    list.classList.toggle("open");
-  };
+document.addEventListener("DOMContentLoaded" , () => {
+  initApp();
+});
 
-  const displayCurrentYear = () => {
-    const year = new Date().getFullYear();
-    document.getElementById("year").textContent = year;
-  };
+
+const initApp = () =>{
+  const menuBtn = document.querySelector(".nav__burger-btn");
+  const list = document.querySelector(".navbar__list");
+  menuBtn.addEventListener("click", toggleButtonMenu)
+  list.addEventListener("click", toggleButtonMenu)
+  displayCurrentYear();
+}
+
+
+const toggleButtonMenu= () =>{
+  const menuBtn = document.querySelector(".nav__burger-btn");
+  const list = document.querySelector(".navbar__list");
+  menuBtn.classList.toggle("open");
+  list.classList.toggle("open");
+}
+
+const displayCurrentYear = () => {
+  const year = new Date().getFullYear();
+  document.getElementById("year").textContent = year;
+};
